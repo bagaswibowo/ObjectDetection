e49da5d2da8dba3da992f3a4fdd83e0e0cc327b2
bagaswibowo <bagaswibowo.2017@student.uny.ac.id>
2025-05-29 23:58:12 +0700

    Fix deployment issues

--- startup.sh
+++ startup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Buat virtual environment jika belum ada
+if [ ! -d "/home/site/wwwroot/antenv" ]; then
+    python -m venv /home/site/wwwroot/antenv
+fi
+
 # Aktifkan virtual environment
 source /home/site/wwwroot/antenv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
 
 # Pre-download model YOLO
 python -c "from ultralytics import YOLO; YOLO('yolov8n.pt')"
